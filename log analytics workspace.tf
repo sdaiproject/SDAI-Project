5e1753858e13fb8e37c1a2b524b653df6f088ead
sdaiproject <77286971+sdaiproject@users.noreply.github.com>
2022-11-18 08:15:49 -0800

    Delete log analytics workspace.tf

--- log analytics workspace.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_log_analytics_workspace" "tf_log_analytics_workspace" {
-  name                = "loganalyticssdai"
-  location            = "East US"
-  resource_group_name = "SDAI-ResourceGroup"
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
