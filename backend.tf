10efcee42a82954c1ff804b382f9420ff97f65a5
sdaiproject <77286971+sdaiproject@users.noreply.github.com>
2022-11-18 08:15:13 -0800

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name ="SDAI-ResourceGroup"
-        storage_account_name ="sdaistorageaccount"
-        container_name ="blob"
-        key = "state.json"
-    }
-}
